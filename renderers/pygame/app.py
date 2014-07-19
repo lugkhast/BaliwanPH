217a1af70b273a2e80fe4ed55ed4be7401782045
Lawrence Patrick Calulo <lugkhast@gmail.com>
2014-07-19 20:46:49 +0800

    Increase resolution, add FPS cap

--- renderers/pygame/app.py
+++ renderers/pygame/app.py
@@ -10,10 +10,11 @@ class BaliwanApplication(object):
         self.city = city
 
     def start(self):
-        self.display_surface = pygame.display.set_mode((640, 480))
+        self.display_surface = pygame.display.set_mode((1024, 600))
         pygame.display.set_caption('BaliwanPH')
         renderer = PygameRenderer(self.city)
 
+        fps_clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
@@ -24,3 +25,4 @@ class BaliwanApplication(object):
             if running:
                 renderer.get_screen(self.display_surface)
                 pygame.display.update()
+                fps_clock.tick(30)
