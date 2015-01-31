2c6d5e7dd05af64f1b09a4b4d0b2638c1b893648
Lawrence Patrick Calulo <lugkhast@gmail.com>
2015-01-31 10:41:34 +0800

    Change rendering to flat 2D

--- renderers/pygame/renderer.py
+++ renderers/pygame/renderer.py
@@ -10,12 +10,11 @@ from .utils.textures import TextureStorage
 
 TOP_OFFSET = 32
 DOODAD_OFFSET_Y = -32
-TILE_SIZE = 64
+TILE_SIZE = 32
 
 class PygameRenderer(object):
     def __init__(self, city):
         self.city = city
-        spritesheet = Spritesheet('assets/terrain_0.png')
 
         self.textures = textures = TextureStorage()
         self._terrain = None
@@ -27,21 +26,9 @@ class PygameRenderer(object):
             ZoneType.INDUSTRIAL: textures.ZONE_TILE_INDUSTRIAL
         }
 
-    def _get_tile_px_offset(self, x, y, surface_width):
-        # The topmost (top-left) tile is centered by default
-        initial_pos_x = surface_width / 2
-
-        # The px calculation is done by first getting the values for the
-        # corresponding tile in the first column, then from there we get the
-        # offset to the tile we're actually calculating for.
-
-        # Position values for the first column
-        base_pos_x = initial_pos_x - (32 * (x + 1))
-        base_pos_y = 16 * x
-
-        # Additional offset for the tile we're actually calculating for
-        pos_x = base_pos_x + (32 * y)
-        pos_y = base_pos_y + (16 * y)
+    def _get_tile_px_offset(self, x, y, city_surface_width):
+        pos_x = x * TILE_SIZE
+        pos_y = y * TILE_SIZE
 
         return (pos_x, pos_y)
 
@@ -93,31 +80,25 @@ class PygameRenderer(object):
         (width, height) = surface.get_size()
         (grid_width, grid_height) = city.dimensions.as_tuple()
 
-        initial_pos_x = width / 2
+        initial_pos_x = 0
 
         surface.fill(pygame.Color(0, 0, 0))
 
         # Render the landscape layer
         terrain = self.get_terrain()
         # Align the terrain texture with the rest of the world, with offset
-        landscape_offset_x = terrain.get_width() / 2 - surface.get_width() / 2
+        landscape_offset_x = 0
         landscape_offset = (offset[0] - landscape_offset_x, offset[1])
         # Blit with the calculated offset
         surface.blit(terrain, landscape_offset)
 
         for x in range(0, grid_width):
-            pos_x = initial_pos_x - (32 * (x + 1)) + offset[0]
-            pos_y = 16 * x + offset[1]
+            pos_x = (x * 32) + offset[0]
             for y in range(0, grid_height):
+                pos_y = (y * 32) + offset[1]
+                
                 # Render the zone layer
                 image = self._get_representation(city.get_tile_at(x, y))
                 surface.blit(image, (pos_x, pos_y))
-
-                # Render the doodad layer
-                # TODO: Implement actual doodads
-
-                # Increment positions
-                pos_x += 32
-                pos_y += 16
 
         return surface
--- renderers/pygame/utils/textures.py
+++ renderers/pygame/utils/textures.py
@@ -1,17 +1,26 @@
 
+from pygame import Surface
+
 from .spritesheet import SizedSpritesheet
 
 class TextureStorage(object):
-    def __init__(self):
-        spritesheet = SizedSpritesheet('assets/terrain_0.png', (64, 64))
+    def create_easy_texture(self, color):
+        surface = Surface((32, 32))
+        surface.fill(color)
 
-        self.BLANK = spritesheet.image_at(0, 0)
-        self.LANDSCAPE_PLAINS = spritesheet.image_at(1, 0)
+    def __init__(self):
+        spritesheet = SizedSpritesheet('assets/flatass.png', (32, 32))
+
+        self.BLANK = spritesheet.image_at(2, 0)
+        self.LANDSCAPE_PLAINS = spritesheet.image_at(2, 0)
         
         self.DOODAD_GRASS_THICK = spritesheet.image_at(4, 11)
         self.DOODAD_BUSH_SMALL = spritesheet.image_at(6, 12)
         self.DOODAD_BUSH_LARGE = spritesheet.image_at(7, 12)
 
-        self.ZONE_TILE_RESIDENTIAL = spritesheet.image_at(0, 15)
-        self.ZONE_TILE_COMMERCIAL = spritesheet.image_at(1, 15)
-        self.ZONE_TILE_INDUSTRIAL = spritesheet.image_at(2, 15)
+        self.ZONE_TILE_RESIDENTIAL = spritesheet.image_at(5, 1)
+        self.ZONE_TILE_COMMERCIAL = spritesheet.image_at(6, 1)
+        self.ZONE_TILE_INDUSTRIAL = spritesheet.image_at(7, 1)
+
+        self.FLAT_GRASS = spritesheet.image_at(1, 0)
+
